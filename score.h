1967db9c0145964204578bea4e1a4991598259e6
MinhNhatTRUONG-2001 <82155278+MinhNhatTRUONG-2001@users.noreply.github.com>
2021-04-09 10:30:55 +0300

    Update score.h

--- score.h
+++ score.h
@@ -6,10 +6,11 @@
 //	1. Constant definition (e.g #define PI 3.1415)
 //	2. Data structure definitions
 //	3. Function prototypes (function declaration)
-
+#define PI 3.14159
 #define MAX 30
 //Added comment
 void input_scores(int[]);
 void evaluation(int[], int[]);
 void classification(int[], int[]);
 void output_results(int[], int[]);
+//added remotely
